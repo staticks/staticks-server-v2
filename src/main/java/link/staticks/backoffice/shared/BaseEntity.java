477718f1bcac08a2d41faba62d52908d7987fb4e
Min1579 <hsm012362@gmail.com>
2022-06-19 16:00:52 +0900

    shared module init

---
+++ src/main/java/link/staticks/backoffice/shared/BaseEntity.java
@@ -0,0 +1,16 @@
+package link.staticks.backoffice.shared;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+}
